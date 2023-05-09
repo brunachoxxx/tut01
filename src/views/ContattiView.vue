2f3aca522394355ca1991864a05fb6d25018ce8e
Bruno Frontini Ganem <brunacho51@gmail.com>
2023-05-09 17:58:16 +0200

    fetchingButton3

--- src/views/ContattiView.vue
+++ src/views/ContattiView.vue
@@ -1,26 +1,55 @@
 <template>
   <div class="contact">
     <h1 class="text-center">{{ $t("contact") }}</h1>
-    <v-btn color="success" @click="">text</v-btn>
+    <v-btn color="success" :disabled="isLoading" @click="handleButtonClick"
+      >text</v-btn
+    >
+    <v-alert
+      v-if="showAlert"
+      type="isError ? red : success"
+      icon="mdi-alert"
+      :value="true"
+    >
+      {{ response }}
+    </v-alert>
   </div>
 </template>
+
 <script lang="ts">
 import Vue from "vue";
-export default Vue.extend({
-  methods: {
-    myPromise() {
-      return new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (Math.random() < 0.5) {
-            resolve(true);
-          } else {
-            reject(false);
-          }
-        }, 5000);
-      });
-    },
+import axios from "axios";
 
-    handleButtonClick() {},
+export default Vue.extend({
+  data: function () {
+    return {
+      isLoading: false,
+      response: {},
+      isError: false,
+      showAlert: false,
+      simulatedError: false,
+      apiR: "https://jsonplaceholder.typicode.com/users/1/todos",
+    };
+  },
+
+  methods: {
+    async handleButtonClick() {
+      this.simulatedError = !this.simulatedError;
+      this.isLoading = true;
+      this.showAlert = true;
+      try {
+        if (this.simulatedError) throw new Error("request failed");
+        let response = await axios(this.apiR);
+        this.response = response;
+      } catch (error) {
+        this.isError = true;
+        this.response = { error };
+      } finally {
+        this.isLoading = false;
+        setTimeout(() => {
+          this.showAlert = false;
+        }, 5000);
+      }
+    },
   },
 });
 </script>
