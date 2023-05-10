b322c16bed88a02e135cc54c168df9cd3e3823fa
Bruno Frontini Ganem <brunacho51@gmail.com>
2023-05-10 16:40:15 +0200

    adding syncfusion

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,13 @@
 <script lang="ts">
 import Vue from "vue";
 import LayoutComp from "@/components/LayoutComp.vue";
+import { GridPlugin } from "@syncfusion/ej2-vue-grids";
+import { ButtonPlugin } from "@syncfusion/ej2-vue-buttons";
+import { DialogPlugin } from "@syncfusion/ej2-vue-popups";
+
+Vue.use(DialogPlugin);
+Vue.use(ButtonPlugin);
+Vue.use(GridPlugin);
 
 export default Vue.extend({
   name: "App",
